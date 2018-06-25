6974c3d326e66fa07948af536d13f44bb6d7ef18
yuki.ieiri <yuki.ieiri@brainpad.co.jp>
2018-06-25 15:49:33 +0900

    modified .gitignore

--- preprocess.py
+++ preprocess.py
@@ -6,12 +6,30 @@ import sklearn.preprocessing as prp
 
 df_org = pd.read_csv("./data/train.csv", header=0, sep=",", encoding='utf-8')
 
+#with open('./pickles/df_org.pkl', 'wb') as opkl:
+#    pickle.dump(df_org, opkl)
+
 print("show head of df_org")
-print(df_org.head())
-
+print(df_org.head(2))
 print("shape of df_org {0}".format(df_org.shape))
+print("columns of df_org are : {0}".format(list(df_org.columns)))
 
-print("columns of df_org are : {0}".format(df_org.columns))
+df_samp = df_org[['SalePrice', 'YrSold', 'MoSold', 'LotArea']]
 
-with open('./pickles/df_org.pkl', 'wb') as opkl:
-    pickle.dump(df_org, opkl)
+print(df_samp.dtypes)
+for col in list(df_samp.columns):
+    if any(list(df_samp[col].isna())):
+        print("is col na? ", list(df_samp[col].isna()))
+    else:
+        print("not na")
+
+df_stat = pd.DataFrame({'mean': df_samp.mean(axis=0),
+                        'vars': df_samp.var(axis=0)})
+
+print("mean and vars are \n{0}".format(df_stat.T))
+
+#with open('./pickles/df_samp.pkl', 'wb') as opkl:
+#    pickle.dump(df_samp, opkl)
+
+
+#end of file
