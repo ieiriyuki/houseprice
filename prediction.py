07e5b3539c2c2df1d16f74129ff372426f3cc5a4
yuki.ieiri <yuki.ieiri@brainpad.co.jp>
2018-06-25 14:21:04 +0900

    incorporated pikle

--- prediction.py
+++ prediction.py
@@ -1,2 +1,9 @@
 #!/usr/local/bin/python
+import pickle
+import sklearn.preprocessing as prp
 from sklearn.linear_model import LinearRegression
+
+with open('./pickles/df_org.pkl', 'rb') as ipkl:
+    df_org = pickle.load(ipkl)
+
+print("shape of df_org ?: {0}".format(df_org.shape))
--- preprocess.py
+++ preprocess.py
@@ -1,8 +1,17 @@
 #!/usr/local/bin/pyton3
+import pickle
+import pandas as pd
+import sklearn.preprocessing as prp
 
-print("hello, world\n")
 
-import sys
-path = sys.path
+df_org = pd.read_csv("./data/train.csv", header=0, sep=",", encoding='utf-8')
 
-print("path is ", path)
+print("show head of df_org")
+print(df_org.head())
+
+print("shape of df_org {0}".format(df_org.shape))
+
+print("columns of df_org are : {0}".format(df_org.columns))
+
+with open('./pickles/df_org.pkl', 'wb') as opkl:
+    pickle.dump(df_org, opkl)
