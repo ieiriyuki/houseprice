93722fa65bd90a2a7f857ea8841f0b8b2d52c20b
yuki.ieiri <yuki.ieiri@brainpad.co.jp>
2018-06-25 17:38:03 +0900

    made a model needed to modify

--- prediction.py
+++ prediction.py
@@ -1,9 +1,34 @@
 #!/usr/local/bin/python
 import pickle
-import sklearn.preprocessing as prp
+import numpy as np
+from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
+from sklearn import metrics as mtr
 
-with open('./pickles/df_org.pkl', 'rb') as ipkl:
-    df_org = pickle.load(ipkl)
+with open('./pickles/nd_dump.pkl', 'rb') as ipkl:
+    nd_dump = pickle.load(ipkl)
 
-print("shape of df_org ?: {0}".format(df_org.shape))
+print("shape of df_samp: {0}".format(nd_dump.shape))
+
+y_data, x_data = np.split(nd_dump, [1], axis=1)
+
+print("y shape", y_data.shape)
+print("x shape", x_data.shape)
+
+x_train, x_test, y_train, y_test = train_test_split(
+            x_data, y_data, test_size=0.3)
+
+lm = LinearRegression()
+lm.fit(x_train, y_train)
+
+print("coef: ", lm.coef_)
+print("intc: ", lm.intercept_)
+
+mse_train = np.sqrt(mtr.mean_squared_error(y_train,
+                lm.predict(x_train)))
+print("mse_train: {0}".format(np.log10(mse_train)))
+mse_test = np.sqrt(mtr.mean_squared_error(y_test,
+                lm.predict(x_test)))
+print("mse_test: {0}".format(np.log10(mse_test)))
+
+# end of file
--- preprocess.py
+++ preprocess.py
@@ -1,6 +1,7 @@
 #!/usr/local/bin/pyton3
 import pickle
 import pandas as pd
+import numpy as np
 import sklearn.preprocessing as prp
 
 
@@ -14,22 +15,38 @@ print(df_org.head(2))
 print("shape of df_org {0}".format(df_org.shape))
 print("columns of df_org are : {0}".format(list(df_org.columns)))
 
-df_samp = df_org[['SalePrice', 'YrSold', 'MoSold', 'LotArea']]
+df_samp = df_org[['SalePrice', 'YrSold', 'MoSold', 'LotArea','BedroomAbvGr']]
 
 print(df_samp.dtypes)
+'''
 for col in list(df_samp.columns):
     if any(list(df_samp[col].isna())):
         print("is col na? ", list(df_samp[col].isna()))
     else:
         print("not na")
+'''
 
 df_stat = pd.DataFrame({'mean': df_samp.mean(axis=0),
                         'vars': df_samp.var(axis=0)})
-
 print("mean and vars are \n{0}".format(df_stat.T))
 
-#with open('./pickles/df_samp.pkl', 'wb') as opkl:
-#    pickle.dump(df_samp, opkl)
+y_data = np.array([list(df_samp['SalePrice'])])
+
+x_data = df_samp.iloc[:,1:]
+
+scaler = prp.StandardScaler()
+scaler.fit(x_data)
+
+x_scal = scaler.transform(x_data)
+
+df_stat = pd.DataFrame({'mean': x_scal.mean(axis=0),
+                        'vars': x_scal.var(axis=0)})
+print("mean and vars are \n{0}".format(df_stat.T))
+
+nd_dump = np.concatenate((y_data.T, x_scal), axis=1)
+
+with open('./pickles/nd_dump.pkl', 'wb') as opkl:
+    pickle.dump(nd_dump, opkl)
 
 
 #end of file
